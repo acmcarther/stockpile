645c26c8ff1bac628376be40453f319e338f8ecf
Alex McArther <acmcarther@gmail.com>
2017-10-12 19:09:46 -0700

    Make InProcessServer::create less panicky (#3)

--- tools/server/src/lib.rs
+++ tools/server/src/lib.rs
@@ -9,7 +9,9 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::path::PathBuf;
+use std::path::Path;
 use std::io::Read;
+use std::io;
 use tools_common::snapshot::WorkspaceSnapshot;
 use tools_common::manifest::WorkspaceManifest;
 use git2::Repository;
@@ -38,11 +40,13 @@ pub struct InProcessServer {
 }
 
 impl InProcessServer {
-  pub fn create(params: ServerParams) -> InProcessServer {
-    InProcessServer {
-      local_state: load_local_repo(&params),
-      params: params,
-    }
+  pub fn create(params: ServerParams) -> Result<InProcessServer, ServerInitErr> {
+    load_local_state(&params).map(|state| {
+      InProcessServer {
+        local_state: state,
+        params: params,
+      }
+    })
   }
 
   pub fn debug(&self) {
@@ -56,56 +60,92 @@ struct LocalServerState {
   manifest: WorkspaceManifest,
 }
 
-fn load_local_repo(params: &ServerParams) -> LocalServerState {
-  // Verify dir exists, or create it
+#[derive(Debug)]
+pub enum ServerInitErr {
+  InvalidCacheState(String),
+  UnderspecifiedPath,
+  GitErr(git2::Error),
+  IoErr(io::Error),
+  SerdeErr(serde_yaml::Error),
+}
+
+impl From<git2::Error> for ServerInitErr {
+  fn from(error: git2::Error) -> ServerInitErr {
+    ServerInitErr::GitErr(error)
+  }
+}
+
+impl From<io::Error> for ServerInitErr {
+  fn from(error: io::Error) -> ServerInitErr {
+    ServerInitErr::IoErr(error)
+  }
+}
+
+impl From<serde_yaml::Error> for ServerInitErr {
+  fn from(error: serde_yaml::Error) -> ServerInitErr {
+    ServerInitErr::SerdeErr(error)
+  }
+}
+
+fn find_or_load_repository(params: &ServerParams) -> Result<Repository, ServerInitErr> {
   match fs::metadata(&params.repo_directory) {
     Ok(m) => {
       if !m.is_dir() {
-        panic!("Tried to load local server state from {}, but it is a file, not a directory!",
-               params.repo_directory.to_str().unwrap_or("[UNRENDERABLE]"))
+        return Err(ServerInitErr::InvalidCacheState(
+              format!("Tried to load local server state from {}, but it is a file, not a directory!",
+                      params.repo_directory.to_str().unwrap_or("[UNRENDERABLE]"))));
       }
+      Repository::init(&params.repo_directory).map_err(ServerInitErr::from)
     },
     Err(_) => {
-      let parent = params.repo_directory.parent().unwrap();
+      let parent = try!(params.repo_directory.parent()
+                        .ok_or(ServerInitErr::UnderspecifiedPath));
       let _ = fs::create_dir_all(&parent);
-      let repo = Repository::clone(&params.remote_repo, parent).unwrap();
+      Repository::clone(&params.remote_repo, parent).map_err(ServerInitErr::from)
     }
-  };
+  }
+}
 
-  let manifest_path = params.repo_directory.join("manifest.yaml");
-  let mut manifest_str = String::new();
-  File::open(manifest_path).unwrap().read_to_string(&mut manifest_str).unwrap();
-  let manifest = serde_yaml::from_str::<WorkspaceManifest>(&manifest_str).unwrap();
+fn load_small_file<P: AsRef<Path>>(path: P) -> Result<String, ServerInitErr> {
+  let mut contents = String::new();
+  try!(File::open(path)
+    .map(|mut f| f.read_to_string(&mut contents))
+    .map_err(ServerInitErr::from));
+  Ok(contents)
+}
+
+fn load_local_state(params: &ServerParams) -> Result<LocalServerState, ServerInitErr> {
+  let repo = try!(find_or_load_repository(&params));
+  let manifest_str = try!(load_small_file(params.repo_directory.join("manifest.yaml")));
+  let manifest = try!(serde_yaml::from_str::<WorkspaceManifest>(&manifest_str)
+                      .map_err(ServerInitErr::from));
 
   let snapshots_dir = params.repo_directory.join("snapshots/");
-  let snapshots = match fs::metadata(&snapshots_dir) {
-    Ok(m) => {
-      if m.is_dir() {
-        fs::read_dir(&snapshots_dir).unwrap()
-          .map(|entry| entry.unwrap())
-          .filter(|entry| match entry.path().file_name() {
-            None => false,
-            Some(file_name) => {
-              let s = file_name.to_str().unwrap();
-              s.starts_with("snapshot") && s.ends_with(".yaml")
-            }
-          }).map(|entry| {
-            let mut snapshot_str = String::new();
-            let file = File::open(entry.path()).unwrap().read_to_string(&mut snapshot_str).unwrap();
-            let snapshot = serde_yaml::from_str::<WorkspaceSnapshot>(&snapshot_str).unwrap();
-            snapshot
-          }).collect::<Vec<_>>()
-      } else {
-        Vec::new()
-      }
-    },
-    Err(_) => {
-      Vec::new()
-    }
-  };
+  let dir_iter = try!(fs::read_dir(&snapshots_dir)
+                      .map_err(ServerInitErr::from));
+  let mut snapshots = Vec::new();
 
-  LocalServerState {
+  for entry_res in dir_iter {
+    // Skip unreadable files
+    if entry_res.is_err() {
+      continue
+    }
+    let entry = entry_res.unwrap();
+    let file_path = entry.path();
+
+    // Skip unknown files
+    if !file_path.starts_with("snapshot") || !file_path.ends_with("yaml") {
+      continue
+    }
+
+    let snapshot_str = try!(load_small_file(&file_path));
+    let snapshot = try!(serde_yaml::from_str::<WorkspaceSnapshot>(&snapshot_str)
+                        .map_err(ServerInitErr::from));
+    snapshots.push(snapshot);
+  }
+
+  Ok(LocalServerState {
     known_snapshots: snapshots,
     manifest: manifest,
-  }
+  })
 }
