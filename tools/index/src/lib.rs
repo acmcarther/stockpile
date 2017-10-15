95461e4262c8af28d430159b8934abb29ab69cd1
Alex McArther <acmcarther@google.com>
2017-10-14 22:35:37 -0700

    WIP

--- tools/index/src/lib.rs
+++ tools/index/src/lib.rs
@@ -6,8 +6,25 @@ extern crate serde_yaml;
 mod _cargo {
   struct IndexEntry {
     pub name: String,
-
+    pub vers: String,
+    pub deps: Vec<DependencyEntry>,
+    pub cksum: String,
+    pub features: FeatureEntry
+    pub yanked: bool
   }
+
+  struct DependencyEntry {
+    pub name: String,
+    pub req: String,
+    pub features: Vec<String>,
+    pub optional: bool,
+    pub default_features: bool,
+    pub target: Optional<String>,
+    pub kind: String,
+  }
+
+  struct FeatureEntry {
+    pub features_per_
 }
 
 #[cfg(test)]
