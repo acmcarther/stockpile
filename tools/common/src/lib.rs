7c5ce10662f7af642e6f4d5fd16c9ad1192b75be
Alex McArther <acmcarther@gmail.com>
2017-10-15 15:23:27 -0700

    wip

--- tools/common/src/lib.rs
+++ tools/common/src/lib.rs
@@ -3,18 +3,58 @@ extern crate serde;
 #[macro_use(Serialize, Deserialize)]
 extern crate serde_derive;
 extern crate serde_yaml;
+extern crate chrono;
 
-pub mod manifest {
+use chrono::DateTime;
+use chrono::Utc;
+
+pub mod cargo {
+  use super::*;
+  use std::collections::HashMap;
+  // Mostly a copy from github/rust-lang/crates.io/src/git.rs
+  // WARNING: On sync from upstream crates.io-index, all modifications
+  // besides the "extra" entry are lost. Add new metadata into "extra".
+  #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+  pub struct IndexEntry {
+    pub name: String,
+    pub vers: String,
+    pub deps: Vec<DependencyEntry>,
+    pub cksum: String,
+    pub features: HashMap<String, Vec<String>>,
+    pub yanked: Option<bool>,
+    pub extra: Option<ExtraEntry>,
+  }
+
+  // Mostly a copy from github/rust-lang/crates.io/src/git.rs
+  #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+  pub struct DependencyEntry {
+    pub name: String,
+    pub req: String,
+    pub features: Vec<String>,
+    pub optional: bool,
+    pub default_features: bool,
+    pub target: Option<String>,
+    pub kind: Option<String>,
+  }
+
+  // Stockpile-specific data added to the index entry
+  #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+  pub struct ExtraEntry {
+    dev_dependencies: Option<Vec<DependencyEntry>>,
+  }
+}
+
+pub mod configuration {
   #[derive(Debug, Clone, Serialize, Deserialize)]
-  pub struct WorkspaceManifest {
-    crates: Vec<MaintainerManifest>,
+  pub struct WorkspaceConfiguration {
+    crate_sets: Vec<MaintainerConfiguration>,
     skip_dev_dependencies: Vec<String>,
   }
 
   #[derive(Debug, Clone, Serialize, Deserialize)]
-  pub struct MaintainerManifest {
+  pub struct MaintainerConfiguration {
     maintainer: String,
-    crats: Vec<String>,
+    crates: Vec<String>,
   }
 }
 
@@ -47,6 +87,13 @@ pub mod snapshot {
     revision: String,
   }
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct WorkspaceMetadata {
+  last_index_time: Option<DateTime<Utc>>,
+  crates_io_index_revision: String,
+}
+
 
 #[cfg(test)]
 mod tests {
--- tools/index/src/bin/run.rs
+++
@@ -1,1 +0,0 @@
-extern crate tools_index;
