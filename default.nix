af41c95263613ad723d4acb9744483231a07062f
Alex McArther <acmcarther@gmail.com>
2017-11-05 13:11:58 -0800

    Add default.nix (containing openssl sysdeps)

---
+++ default.nix
@@ -0,0 +1,21 @@
+# TODO: Untangle this crazy business
+with (import <nixpkgs> {});
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.stdenv.mkDerivation rec {
+  name = "package";
+  propagatedBuildInputs = [
+    pkgs.vulkan-loader
+    pkgs.inotify-tools
+    pkgs.bash
+    pkgs.patchelf
+    pkgs.openssl.dev
+    pkgs.pkgconfig
+    pkgs.zlib
+    pkgs.cmake] ;
+  shellHook = ''
+    # Allow my shell to add custom snippet
+    export IS_NIX_SHELL=1
+    export BAZEL_SH=/run/current-system/sw/bin/bash
+  '';
+}
