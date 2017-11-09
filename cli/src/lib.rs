269e2f5c29a3b62bc95bc7e1e892e14c4d019e5a
Alex McArther <acmcarther@gmail.com>
2017-11-09 11:14:58 -0800

    Gut CLI (after removing server)

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -3,11 +3,8 @@ extern crate serde;
 #[macro_use(Serialize, Deserialize)]
 extern crate serde_derive;
 extern crate serde_yaml;
-extern crate server;
 
 pub mod commands {
-  use server::InProcessServer;
-  use server::ServerParamsBuilder;
   use std::path::PathBuf;
   use std::env;
 
@@ -15,40 +12,15 @@ pub mod commands {
     pub repo_directory: Option<PathBuf>,
   }
 
-  impl <'a> From<&'a SnapshotNowParams> for ServerParamsBuilder {
-    fn from(params: &'a SnapshotNowParams) -> ServerParamsBuilder {
-      let mut server_params_builder = ServerParamsBuilder::default();
-      if let Some(ref repo_dir) = params.repo_directory {
-        server_params_builder.repo_directory(repo_dir.clone());
-      }
-      server_params_builder
-    }
-  }
-
   pub fn snapshot_now(params: SnapshotNowParams) {
-    let server_params = ServerParamsBuilder::from(&params).build().unwrap();
-    let server = InProcessServer::create(server_params);
   }
 
   pub struct SyncIndexParams{
     pub repo_directory: Option<PathBuf>,
   }
 
-  impl <'a> From<&'a SyncIndexParams> for ServerParamsBuilder {
-    fn from(params: &'a SyncIndexParams) -> ServerParamsBuilder {
-      let mut server_params_builder = ServerParamsBuilder::default();
-      if let Some(ref repo_dir) = params.repo_directory {
-        server_params_builder.repo_directory(repo_dir.clone());
-      }
-      server_params_builder
-    }
-  }
 
   pub fn sync_index(params: SyncIndexParams) {
-    let server_params = ServerParamsBuilder::from(&params).build().unwrap();
-    let server = InProcessServer::create(server_params);
-
-    server.synchronize_index();
   }
 
   pub struct QueryParams {
